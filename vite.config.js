432a6f0f7cbc02c03d5635c720b9e7aa66474031
viteledev <agnaldotgt@gmail.com>
2026-02-08 21:39:14 -0300

    Add gh-pages deploy scripts

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,12 @@ import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig(({ mode }) => {
+export default defineConfig(({ mode, command }) => {
   const env = loadEnv(mode, process.cwd(), '')
+  const defaultBase =
+    command === 'build' ? '/pratika-ferragista-online/' : '/'
   return {
-    base: env.VITE_BASE_PATH || '/',
+    base: env.VITE_BASE_PATH || defaultBase,
     plugins: [react()],
   }
 })
